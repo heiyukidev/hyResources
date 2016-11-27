05fff701a41fd8c684e091be1c2d60745b7fbb58
heiyukidev <heiyukidev@gmail.com>
2016-11-27 19:50:38 +0100

    restructuring

--- app.js
+++
@@ -1,8 +0,0 @@
-(function(angular){
-    function runFn($hyResources){
-        //Initializing the local Storage for 
-        localStorage.hyResources = JSON.stringify([]);
-    }
-    runFn.$inject = ['$hyResources']
-    angular.module("hyResources",['ngResource']).run(runFn);
-})(angular);
